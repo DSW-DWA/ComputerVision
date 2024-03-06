7277b0881a07b1d0c5de2f6065c67e11b5267f2a
ZetoOfficial <tpptgs@bk.ru>
2024-03-06 21:49:49 +0500

    vue.js первый и третий пункт

---
+++ image-craft/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ImageProcessor />
+  </div>
+</template>
+
+<script>
+import ImageProcessor from './components/ImageProcessor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ImageProcessor
+  }
+}
+</script>
