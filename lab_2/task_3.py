3cbefe32560e4127d89641a33234255c0c5ab0f5
andreych <cad.dsw.dwa@gmail.com>
2024-03-23 13:24:05 +0500

    fin task 3

--- lab_2/task_3.py
+++ lab_2/task_3.py
@@ -1,15 +1,15 @@
 import numpy as np
 from PIL import Image
 
-from task_2 import load_image, rectangular_filter
+from task_2 import load_image, rectangular_filter, gaussian_filter, median_filter
 
 
-def unsharp_masking(image_array, k, lambda_coefficient):
+def unsharp_masking(image_array, filter_coefficients, lambda_coefficient, filter):
     """
     Применяет нерезкое маскирование к изображению для повышения резкости.
     """
     # сглаживание
-    smoothed_image = np.array(rectangular_filter(image_array, k))
+    smoothed_image = np.array(filter(image_array, filter_coefficients))
     # J(p) = (1 + λ)I(p) - λ*S(p) добавление усиленного детального компонента к исходному изображению для повышения резкости
     sharpened_image_array = (1 + lambda_coefficient)*np.array(image_array) - lambda_coefficient * smoothed_image
     # J′(p) = clip(J(p),0,255) ограничение значений пикселей полученного изображения диапазоном [0, 255]
@@ -19,5 +19,9 @@ def unsharp_masking(image_array, k, lambda_coefficient):
 
 if __name__ == '__main__':
     input_image = load_image("../assets/image_bw.jpg")
-    sharp_image = unsharp_masking(input_image, k=3, lambda_coefficient=1.5)
-    sharp_image.save('task_3_result/sharp_image.jpg')
+    sharp_image = unsharp_masking(input_image, filter_coefficients=3, lambda_coefficient=1.5, filter=rectangular_filter)
+    sharp_image.save('task_3_result/sharp_image_rect.jpg')
+    sharp_image = unsharp_masking(input_image, filter_coefficients=1.2, lambda_coefficient=1.5, filter=gaussian_filter)
+    sharp_image.save('task_3_result/sharp_image_gauss.jpg')
+    sharp_image = unsharp_masking(input_image, filter_coefficients=3, lambda_coefficient=1.5, filter=median_filter)
+    sharp_image.save('task_3_result/sharp_image_median.jpg')
